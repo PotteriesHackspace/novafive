383441a414c6e894f6721abcca3b2f1baca800b9
Benjamin James Harrison-Sims <tehrainbowguy@gmail.com>
2015-10-16 13:47:09 +0100

    Cleanup and fix responders.

--- src/main/java/org/potterieshackspace/novafive/listeners/HelloListener.java
+++ src/main/java/org/potterieshackspace/novafive/listeners/HelloListener.java
@@ -9,33 +9,37 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class HelloListener extends ListenerAdapter {
     private final HashMap<String, String> responders;
     public HelloListener() {
         responders = new HashMap<>();
+        reloadResponders();
+    }
+
+    private void reloadResponders(){
         try {
             Stream<String> lines = Files.lines(Paths.get("responders.txt"));
-            lines.forEach(x -> responders.put(x.split(":", 2)[0], x.split(":", 2)[0]));
+            lines.forEach(x -> responders.put(x.split(":", 2)[0], x.split(":", 2)[1]));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    //idiot split on first space
     @Override
     public void onGenericMessage(GenericMessageEvent event) {
+        String message = event.getMessage().split(" ", 2)[0];
         //When someone says ?helloworld respond with "Hello World"
-        if (event.getMessage().startsWith("?helloworld")) {
+        if (message.equalsIgnoreCase("?helloworld")) {
             event.respond("Hello world!");
-        } else if (event.getMessage().startsWith("?respond")){
+        } else if (message.equalsIgnoreCase("?respond")){
             String[] split = event.getMessage().split(" ", 3);
             if (split.length == 3) {
                 responders.put(split[1], split[2]);
                 List<String> strings = new ArrayList<>();
-                responders.forEach((x, y) -> {
-                    strings.add(String.format("%s:%s", x, y));
-                });
+                responders.forEach((x, y) -> strings.add(String.format("%s:%s", x, y)));
                 try {
                     Files.write(Paths.get("responders.txt"), strings);
                 } catch (IOException e) {
@@ -44,12 +48,16 @@ public class HelloListener extends ListenerAdapter {
 
                 event.respond("Responder added: " + split[1] + " - " + split[2]);
             }
-        } else {
-            responders.keySet().stream().forEach(x -> {
-                if (event.getMessage().startsWith(x)) {
-                    event.respond(responders.get(x));
-                }
-            });
+        } else if(message.equalsIgnoreCase("?responders")){
+            final String[] response = {"Responders: "};
+            responders.keySet().stream().map(s -> s + ", ").forEachOrdered(s1 -> response[0] +=s1);
+            event.respond(response[0].substring(0, response[0].length() - 2));
+        } else if (message.equalsIgnoreCase("?reloadresponders")) {
+            reloadResponders();
+            event.respond("Done!");
+        }else {
+            Optional<String> s = responders.keySet().stream().filter(s3 -> s3.equalsIgnoreCase(message)).findAny();
+            s.ifPresent(x -> event.respond(responders.get(x.toLowerCase())));
         }
     }
 }
